0d41aa2aca4b105a4326b0a5fe12664f61326df9
rory <rory@expensify.com>
2023-01-06 11:23:27 -0800

    Use reportID as key for WishList.Component

--- src/components/List/index.ios.js
+++ src/components/List/index.ios.js
@@ -26,13 +26,15 @@ export default function List({data, report, ...props}) {
 
     const ref = useRef();
 
+    const prefix = report.reportID
+
     useEffect(() => {
         ref.current?.update((dataCopy) => {
             'worklet'
 
             // just update data - big re-render.
             for (const i of data) {
-                dataCopy.set(i.key, i)
+                dataCopy.set(prefix + i.reportActionID, i)
             }
         })
     }, [data])
@@ -40,7 +42,7 @@ export default function List({data, report, ...props}) {
 
     data.map((d) => console.log(d))
 
-    const preparedData = data.map(it => ({type: 'test', key: it.reportActionID, ...it})); // only for testing
+    const preparedData = data.map(it => ({type: 'test', key: prefix + it.reportActionID, ...it})); // only for testing
 
 
     const [,dummy] = useState(false)
@@ -52,6 +54,7 @@ return () => clearInterval(i)
     return (
         <View style={styles.container}>
             <Wishlist.Component
+                key={prefix}
                 style={styles.list}
                 initialIndex={preparedData.length - 1} // aka inverted
                 initialData={preparedData}
