9a1dd14adfe3f64d59b47eb1ba667d8d0906a115
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-09-19 17:40:04 +0200

    fix index

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-require('./src/libs/E2E/reactNativeLaunchingTest');
+/**
+ * @format
+ */
+
+import {enableLegacyWebImplementation} from 'react-native-gesture-handler';
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import Config from './src/CONFIG';
+import additionalAppSetup from './src/setup';
+
+enableLegacyWebImplementation(true);
+AppRegistry.registerComponent(Config.APP_NAME, () => App);
+additionalAppSetup();
