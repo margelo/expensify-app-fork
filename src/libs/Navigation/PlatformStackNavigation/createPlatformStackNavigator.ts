7fd51a05dbf861b2f9b87f785c342d379285f3fb
kirillzyusko <kiryl@margelo.io>
2024-03-21 16:13:08 +0100

    chore: begin web testing

--- src/libs/Navigation/PlatformStackNavigation/createPlatformStackNavigator.ts
+++ src/libs/Navigation/PlatformStackNavigation/createPlatformStackNavigator.ts
@@ -1,3 +1,4 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const createPlatformStackNavigator: typeof createStackNavigator = () => createStackNavigator();
