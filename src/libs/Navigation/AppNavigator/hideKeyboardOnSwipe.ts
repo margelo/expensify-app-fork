2380300ba9ed6d8c9c78e8a06febd6fdbf3535c4
Christoph Pader <chris@margelo.io>
2024-04-16 18:14:20 +0200

    add more types

--- src/libs/Navigation/AppNavigator/hideKeyboardOnSwipe.ts
+++ src/libs/Navigation/AppNavigator/hideKeyboardOnSwipe.ts
@@ -1,7 +1,9 @@
+import type {StackNavigationOptions} from '@react-navigation/stack';
+
 const hideKeyboardOnSwipe = {
     // temporary solution - better to hide a keyboard than see keyboard flickering
     // see https://github.com/software-mansion/react-native-screens/issues/2021 for more details
     keyboardHandlingEnabled: true,
-};
+} satisfies StackNavigationOptions;
 
 export default hideKeyboardOnSwipe;
